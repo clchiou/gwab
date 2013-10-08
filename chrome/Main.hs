e2b2efae2dd06d35dc01c8598b58a2a2b4b0df15
Che-Liang Chiou <clchiou@gmail.com>
2013-10-07 23:06:31 -0700

    Add ffi for resolving domain name and create socket connection

--- chrome/Main.hs
+++ chrome/Main.hs
@@ -3,14 +3,38 @@
 module Main where
 
 import Haste
+import Haste.Prim
 
 import Telnet
 
 foreign import ccall js_logging :: JSString -> IO ()
 
+foreign import ccall js_resolve :: JSString ->
+                                   JSFun (JSString -> IO ()) ->
+                                   IO ()
+
+foreign import ccall js_connect :: JSString ->
+                                   Int ->
+                                   JSFun (Int -> Int -> IO ()) ->
+                                   IO ()
+
 
 logging :: String -> IO ()
-logging = js_logging . toJSString
+logging = js_logging . toJSStr
 
 
-main = logging "Hello world"
+resolve :: String -> (String -> IO ()) -> IO ()
+resolve host cb = js_resolve (toJSString host) (mkCallback js_cb)
+    where js_cb = cb . fromJSStr
+
+
+connect :: String -> Int -> (Int -> Int -> IO ()) -> IO ()
+connect addr port cb = js_connect (toJSStr addr) port (mkCallback cb)
+
+
+main = do
+    logging "Hello world"
+    resolve "ptt.cc" (\addr -> connect addr 23 showConnect)
+    where showConnect socketId resultCode =
+            logging $ "socketId="   ++ (show socketId) ++ ", " ++
+                      "resultCode=" ++ (show resultCode)
--- chrome/wrapper.js
+++ chrome/wrapper.js
@@ -1,6 +1,35 @@
 // Copyright (C) 2013 Che-Liang Chiou.
 
 
+var socket = chrome.socket || chrome.experimental.socket;
+
+// TODO(clchiou): Be sure that chrome.experimental.dns is not removed in
+// future version of Chrome.
+var dns = chrome.experimental.dns;
+
+
 function js_logging(message) {
   console.log(message);
+}
+
+
+function js_resolve(host, callback) {
+  dns.resolve(host, function(result) {
+    A(callback, [[0, result.address], 0]);
+  });
+}
+
+
+function js_connect(addr, port, callback) {
+  socket.create('tcp', {}, function (createInfo) {
+    var socketId = createInfo.socketId;
+    if (socketId > 0) {
+      socket.connect(socketId, addr, port, function (resultCode) {
+        A(callback, [[0, socketId], [0, resultCode], 0]);
+      });
+    } else {
+      console.log('js_connect: Could not create socket');
+      A(callback, [[0, socketId], [0, 0], 0]);
+    }
+  });
 }
